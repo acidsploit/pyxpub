384d89e203c9137ea0b8d5d0a2c0b10d8ad26dd2
acidsploit <acidsploit@users.noreply.github.com>
2018-01-31 15:10:51 +0100

    Create script.js

---
+++ examples/jquery/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $.getJSON( "http://localhost:8080/payment?amount=0.0023&label=SHOP:1Wed2B44", function( data ) {
+    var items = [];
+    var payment = data['payment'];
+    $.each( payment, function( key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+  
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
+}); 
