aa4942036011b522588ccdbac4b70577ff3d290f
acidsploit <acidsploit@users.noreply.github.com>
2018-02-21 12:14:07 +0100

    Delete script.js

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
